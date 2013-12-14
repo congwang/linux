941b6ab47c6a164a828fb126f34e893fe3dea017
Cong Wang <xiyou.wangcong@gmail.com>
2013-12-13 19:23:40 -0800

    ppp: use skb queue API
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>

--- drivers/net/ppp/ppp_generic.c
+++ drivers/net/ppp/ppp_generic.c
@@ -2077,7 +2077,7 @@ ppp_mp_reconstruct(struct ppp *ppp)
 
 	if (ppp->mrru == 0)	/* do nothing until mrru is set */
 		return NULL;
-	head = list->next;
+	head = skb_peek(list);
 	tail = NULL;
 	skb_queue_walk_safe(list, p, tmp) {
 	again:
