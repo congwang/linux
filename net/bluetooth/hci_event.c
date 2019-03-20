bbaad4c7a12f16f30aba37213faa4ade573827f9
Cong Wang <xiyou.wangcong@gmail.com>
2019-03-20 13:54:15 -0700

    bluetooth: validate HCI_EV_SYNC_CONN_COMPLETE packet carefully
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -4145,8 +4145,12 @@ unlock:
 static void hci_sync_conn_complete_evt(struct hci_dev *hdev,
 				       struct sk_buff *skb)
 {
-	struct hci_ev_sync_conn_complete *ev = (void *) skb->data;
+	struct hci_ev_sync_conn_complete *ev;
 	struct hci_conn *conn;
+
+	if (unlikely(!pskb_may_pull(skb, sizeof(*ev))))
+		return;
+	ev = (void *)skb->data;
 
 	BT_DBG("%s status 0x%2.2x", hdev->name, ev->status);
 
