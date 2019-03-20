48c7bafdfacf65dc14800377a49b37c80d2b8440
Cong Wang <xiyou.wangcong@gmail.com>
2019-03-20 13:03:47 -0700

    bluetooth: validate HCI_EV_ENCRYPT_CHANGE packet carefully
    Similarly, we need to check skb->data boundary for
    HCI_EV_ENCRYPT_CHANGE too.
    
    Cc: Marcel Holtmann <marcel@holtmann.org>
    Cc: Johan Hedberg <johan.hedberg@gmail.com>
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -2915,8 +2915,12 @@ unlock:
 
 static void hci_encrypt_change_evt(struct hci_dev *hdev, struct sk_buff *skb)
 {
-	struct hci_ev_encrypt_change *ev = (void *) skb->data;
+	struct hci_ev_encrypt_change *ev;
 	struct hci_conn *conn;
+
+	if (unlikely(!pskb_may_pull(skb, sizeof(*ev))))
+		return;
+	ev = (void *)skb->data;
 
 	BT_DBG("%s status 0x%2.2x", hdev->name, ev->status);
 
