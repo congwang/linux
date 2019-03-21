14cc0f431cc746bdd98907d584f2037ac6baa89a
Cong Wang <xiyou.wangcong@gmail.com>
2019-03-20 18:25:06 -0700

    bluetooth: validate HCI_EV_USER_PASSKEY_REQUEST packet carefully
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -4576,10 +4576,13 @@ unlock:
 static void hci_user_passkey_request_evt(struct hci_dev *hdev,
 					 struct sk_buff *skb)
 {
-	struct hci_ev_user_passkey_req *ev = (void *) skb->data;
+	struct hci_ev_user_passkey_req *ev;
 
 	BT_DBG("%s", hdev->name);
 
+	if (unlikely(!pskb_may_pull(skb, sizeof(*ev))))
+		return;
+	ev = (void *)skb->data;
 	if (hci_dev_test_flag(hdev, HCI_MGMT))
 		mgmt_user_passkey_request(hdev, &ev->bdaddr, ACL_LINK, 0);
 }
