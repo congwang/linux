4e5224b6549e17b58d8c5d235aaa02676130ae86
Cong Wang <xiyou.wangcong@gmail.com>
2019-03-20 21:56:30 -0700

    bluetooth: validate HCI_EV_MODE_CHANGE packet carefully
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -3741,8 +3741,12 @@ static void hci_num_comp_blocks_evt(struct hci_dev *hdev, struct sk_buff *skb)
 
 static void hci_mode_change_evt(struct hci_dev *hdev, struct sk_buff *skb)
 {
-	struct hci_ev_mode_change *ev = (void *) skb->data;
+	struct hci_ev_mode_change *ev;
 	struct hci_conn *conn;
+
+	if (unlikely(!pskb_may_pull(skb, sizeof(*ev))))
+		return;
+	ev = (void *)skb->data;
 
 	BT_DBG("%s status 0x%2.2x", hdev->name, ev->status);
 
